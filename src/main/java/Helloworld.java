8c43d72bdbdea558619cd8dd9822efb5b3b7fbe6
yuyingying666 <1834621863@qq.com>
2020-10-05 17:11:23 +0800

    sdfgh1

--- src/main/java/Helloworld.java
+++ src/main/java/Helloworld.java
@@ -1,6 +1,6 @@
 public class Helloworld {
     public static void main(String[] args) {
-        System.out.println("helloworld!");
+        System.out.println("world!");
         System.out.println("hello");
     }
 }
